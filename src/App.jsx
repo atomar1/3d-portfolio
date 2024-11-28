973aa3a021eec287d999abe683b53e2f6916f81d
atomar1 <ansh06tomar@gmail.com>
2024-11-28 13:48:17 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
 import Projects from './sections/Projects.jsx';
-import WorkExperience from './sections/Experience.jsx';
 import NewHero from './sections/NewHero.jsx';
 
 const App = () => {
