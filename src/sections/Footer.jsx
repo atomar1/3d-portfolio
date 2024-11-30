ab99625d224c041d21c59930663499e2a85c7f8b
Ansh Tomar <89928686+atomar1@users.noreply.github.com>
2024-11-30 00:12:04 +0000

    assets added

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
         <div className="flex gap-3 justify-center">
           <div className="social-icon flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 hover:bg-gray-300 transition-colors duration-200">
             <a href="https://github.com/atomar1" target="_blank" rel="noopener noreferrer">
-              <img src="/assets/github.svg" alt="github" className="w-6 h-6 transition-colors duration-200 hover:fill-white" />
+              <img src="/3d-portfolio/public/assets/github.svg" alt="github" className="w-6 h-6 transition-colors duration-200 hover:fill-white" />
             </a>
           </div>
           <div className="social-icon flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 hover:bg-gray-300 transition-colors duration-200">
             <a href="https://www.linkedin.com/in/anshtomar/" target="_blank" rel="noopener noreferrer">
-              <img src="/assets/linkedin.svg" alt="linkedin" className="w-6 h-6 transition-colors duration-200 hover:fill-white" />
+              <img src="/3d-portfolio/public/assets/linkedin.svg" alt="linkedin" className="w-6 h-6 transition-colors duration-200 hover:fill-white" />
             </a>
           </div>
         </div>
